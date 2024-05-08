b69f21f5e5e58a3fe4df36664370c87b379a56c2
rdftgyhuji6hy7 <js1shinda44@hotmail.com>
2024-05-08 09:49:06 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function countConsonants(str) {
-  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
-}
+const pullAtIndex = (arr, pullArr) => {
+  let removed = [];
+  let pulled = arr
+    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
+    .filter((v, i) => !pullArr.includes(i));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+  return removed;
+};
